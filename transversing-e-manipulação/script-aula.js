4c464c08701814463ca9c16ed984d39168e961fc
GabrielH-NH <gabrielhenriquenh@gmail.com>
2023-06-28 21:53:56 -0300

    aprendendo sobre transversing

---
+++ transversing-e-manipulação/script-aula.js
@@ -0,0 +1,47 @@
+// OUTERHTML, INNERHTML E INNERTEXT
+// Propriedades que retornam uma string contendo o html ou texto. É possível atribuir um novo valor para as mesmas element.innerText = 'Novo Texto'.
+
+const menu = document.querySelector('.menu');
+
+menu.outerHTML; // todo o html do elemento
+menu.innerHTML; // html interno
+menu.innerText; // texto, sem tags
+
+menu.innerText = '<p>Texto</p>'; // a tag vai como texto
+menu.innerHTML = '<p>Texto</p>'; // a tag é renderizada
+
+
+
+
+// TRANSVERSING
+// Como navegar pelo DOM, utilizando suas propriedades e métodos.
+
+const lista = document.querySelector('.animais-lista');
+
+lista.parentElement; // pai
+lista.parentElement.parentElement; // pai do pai
+lista.previousElementSibling; // elemento acima
+lista.nextElementSibling; // elemento abaixo
+
+lista.children; // HTMLCollection com os filhos
+lista.children[0]; // primeiro filho
+lista.children[--lista.children.length]; // último filho
+
+lista.querySelectorAll('li'); // todas as LI's
+lista.querySelector('li:last-child'); // último filho
+
+
+
+
+// ELEMENT VS NODE
+// Element's represetam um elemento html, ou seja, uma tag. Node representa um nó, e pode ser um elemento (Element), texto, comentário, quebra de linha e mais.
+
+const lista2 = document.querySelector('.animais-lista');
+
+lista2.previousElementSibling; // elemento acima
+lista2.previousSibling; // node acima
+
+lista2.firstChild; // primeiro node child
+lista2.childNodes; // todos os node child
+
+/* Geralmente estamos atrás de um elemento e não de qualquer node em si. */
